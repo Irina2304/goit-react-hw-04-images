094cbdaa0aaa984a95c086e2e0e098753832e5c6
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-10-20 21:03:24 +0200

    prettier

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,32 +1,31 @@
-import { Search, SearchForm, SearchFormButton, SearchFormInput } from "./Searchbar.styled"
+import { Search, SearchForm, SearchFormButton, SearchFormInput } from './Searchbar.styled';
 import { FaSearch } from 'react-icons/fa';
 
 export const Searchbar = ({ getValue }) => {
+  const onSubmit = evt => {
+    evt.preventDefault();
+    const value = evt.currentTarget.elements.input.value;
+    getValue(value);
+  };
+  return (
+    <Search className="searchbar">
+      <SearchForm onSubmit={onSubmit} className="form">
+        <SearchFormButton type="submit" className="button">
+          <span className="button-label">
+            {' '}
+            <FaSearch />
+          </span>
+        </SearchFormButton>
 
-    const onSubmit = (evt) => {
-        evt.preventDefault();
-        const value = evt.currentTarget.elements.input.value;
-        getValue(value);
-    }
-    return (
-        <Search className="searchbar">
-            <SearchForm onSubmit={onSubmit} className="form">
-                <SearchFormButton type="submit" className="button">
-                    <span className="button-label"> <FaSearch /></span>
-                </SearchFormButton>
-
-                <SearchFormInput
-                className="input"
-                name="input"
-                type="text"
-                autoComplete="off"
-                autoFocus
-                placeholder="Search images and photos"
-                />
-            </SearchForm>
-        </Search>
-
-    )
-}
-
-
+        <SearchFormInput
+          className="input"
+          name="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </Search>
+  );
+};
